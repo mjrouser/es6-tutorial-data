1fc58196b19cdc0a9f5ea1349b66fdc92bac5d90
mjrouser <matthew@mjrouser.com>
2017-08-09 07:33:09 -0400

    webpack and babel setup

--- js/app.js
+++ js/app.js
@@ -1,35 +1,36 @@
 // XMLHttpRequest wrapper using callbacks
-function request(obj, successHandler, errorHandler) {
-    var xhr = new XMLHttpRequest();
+ function request(obj, successHandler, errorHandler) {
+    let xhr = new XMLHttpRequest();
     xhr.open(obj.method || "GET", obj.url);
     if (obj.headers) {
-        Object.keys(obj.headers).forEach(function(key) {
+        Object.keys(obj.headers).forEach((key) => {
             xhr.setRequestHeader(key, obj.headers[key]);
         });
     }
-    xhr.onload = function() {
+    xhr.onload = () => {
         if (xhr.status >= 200 && xhr.status < 300) {
             successHandler(xhr.response);
         } else {
             errorHandler(xhr.statusText);
         }
     };
-    xhr.onerror = function() {
+    xhr.onerror = () => {
         errorHandler(xhr.statusText);
     };
     xhr.send(obj.body);
 }
 
 request({url:"employees.json"},
-    function(data) {
-        var employees = JSON.parse(data);
-        var html = "";
-        employees.forEach(function(employee){
-            html += "<div><img src='" + employee.picture + "'/><div>" + employee.firstName + " " + employee.lastName + "<p>" + employee.phone + "</p></div></div>";
+    (data) => {
+        let employees = JSON.parse(data);
+        let html = "";
+        employees.forEach((employee) => {
+            //npm install babel-core babel-loader babel-preset-es2015 webpack --save-devhtml += "<div><img src='" + employee.picture + "'/><div>" + employee.firstName + " " + employee.lastName + "<p>" + employee.phone + "</p></div></div>";
+            html += `<div><img src="${employee.picture}"/><div>${employee.firstName} ${employee.lastName}<p>${employee.phone}</p></div></div>`;
         });
         document.getElementById("list").innerHTML = html;
     },
-    function(error) {
+    (error) => {
         console.log(error);
     }
 );
