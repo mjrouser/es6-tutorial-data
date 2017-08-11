8055ac03b46ae74bc45265c30864ef92c9b88f17
Matthew R <matthew@mjrouser.com>
2017-08-11 13:11:52 -0400

    completed code

--- js/app.js
+++ js/app.js
@@ -1,36 +1,19 @@
-// XMLHttpRequest wrapper using callbacks
- function request(obj, successHandler, errorHandler) {
-    let xhr = new XMLHttpRequest();
-    xhr.open(obj.method || "GET", obj.url);
-    if (obj.headers) {
-        Object.keys(obj.headers).forEach((key) => {
-            xhr.setRequestHeader(key, obj.headers[key]);
-        });
-    }
-    xhr.onload = () => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            successHandler(xhr.response);
-        } else {
-            errorHandler(xhr.statusText);
-        }
-    };
-    xhr.onerror = () => {
-        errorHandler(xhr.statusText);
-    };
-    xhr.send(obj.body);
-}
+import * as service from "./employee-service-mock";
 
-request({url:"employees.json"},
-    (data) => {
-        let employees = JSON.parse(data);
+service.findAll()
+    .then(employees => {
         let html = "";
         employees.forEach((employee) => {
             //npm install babel-core babel-loader babel-preset-es2015 webpack --save-devhtml += "<div><img src='" + employee.picture + "'/><div>" + employee.firstName + " " + employee.lastName + "<p>" + employee.phone + "</p></div></div>";
-            html += `<div><img src="${employee.picture}"/><div>${employee.firstName} ${employee.lastName}<p>${employee.phone}</p></div></div>`;
+            html += `
+               <div>
+                 <img src="${employee.picture}"/>
+                 <div>${employee.firstName} ${employee.lastName}
+                 <p>${employee.phone}</p>
+                 </div>
+               </div>`;
         });
         document.getElementById("list").innerHTML = html;
-    },
-    (error) => {
-        console.log(error);
-    }
-);
+    })
+    .catch(error => console.log(error)
+    );
